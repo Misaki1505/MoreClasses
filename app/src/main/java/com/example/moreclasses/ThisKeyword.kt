4d45ebdf831da570b18d941b9127015ff7efb35b
Misaki1505 <sawako1505@gmail.com>
2023-01-11 19:38:50 -0500

    Changes 11/01/2022

---
+++ app/src/main/java/com/example/moreclasses/ThisKeyword.kt
@@ -0,0 +1,16 @@
+package com.example.moreclasses
+
+fun main(args: Array<String>) {
+    val myBox = Box()
+    println("Contents is ${myBox.contents}")
+    myBox.updateContents("Jewellery")
+    println("Contents is ${myBox.contents}")
+}
+
+class Box {
+    var contents: String = ""
+
+    fun updateContents(contents: String) {
+        this.contents = contents
+    }
+}
